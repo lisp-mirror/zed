82393b7972b6fd7d6ff9665fef4defe995103fd8
Michael Fiano <mail@mfiano.net>
2021-03-07 19:16:18 -0500

    Improve some comments.

--- src/protocol/core.lisp
+++ src/protocol/core.lisp
@@ -16,7 +16,7 @@
 
 (in-package #:%zed.protocol.core)
 
-;; The entry point of the engine. This constructs a context using any of the optional user-supplied
+;; The entry point of the engine. This constructs a context using the optional user-supplied
 ;; arguments, and then enters the main game loop.
 (defun start-game (&rest options)
   ;; Create the context, initialized with any user-supplied options.
@@ -28,15 +28,18 @@
       ;; and shut everything down.
       (ctx::destroy context))))
 
-;; Stop the currently running context.
+;; Stop the game associated with the given context.
 (defun stop-game (context)
   (ctx::shutdown context))
 
+;; Pause the game associated with the given context. Only game objects that are marked as pausable
+;; stop updating, allowing menus and any other game objects that wish to be interactive to be so.
 (defun pause-game (context)
   (tree::walk-tree (x (ctx::scene-tree context))
     (when (eq (gob::pause-mode x) :pause)
       (setf (gob::paused-p x) t))))
 
+;; Un-pauses the game associated with the given context.
 (defun unpause-game (context)
   (tree::walk-tree (x (ctx::scene-tree context) :paused-p t)
     (when (eq (gob::pause-mode x) :pause)
