c9bef5406e4d0ab66a5eb400820b021108d19d15
Michael Fiano <mail@mfiano.net>
2021-03-27 22:11:52 -0400

    Forgot to add audio file in previous commit

---
+++ src/audio.lisp
@@ -0,0 +1,23 @@
+(in-package #:cl-user)
+
+(defpackage #:%zed.audio
+  ;; Third-party aliases
+  (:local-nicknames
+   (#:log #:verbose)
+   (#:u #:golden-utils))
+  (:use #:cl))
+
+(in-package #:%zed.audio)
+
+(defun start ()
+  (sdl2:init* '(:audio))
+  (sdl2-mixer:init :flac :ogg)
+  (sdl2-mixer:open-audio 44100 :s16sys 2 1024)
+  (sdl2-mixer:allocate-channels 16)
+  (log:info :zed.audio "Started audio system"))
+
+(defun stop ()
+  (sdl2-mixer:halt-channel -1)
+  (sdl2-mixer:close-audio)
+  (sdl2-mixer:quit)
+  (log:info :zed.audio "Stopped audio system"))
