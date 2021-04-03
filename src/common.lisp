c3c6a93e9606ab72f6b7378fffdb88d03f795e66
Michael Fiano <mail@mfiano.net>
2021-04-03 11:38:06 -0400

    with-profiling only expands to SBCL-specific code on SBCL

--- src/common.lisp
+++ src/common.lisp
@@ -22,21 +22,25 @@
              (%gl:pop-debug-group))))))
 
 (defmacro with-profiling ((enable-p) &body body)
-  (let ((packages (remove-if-not
-                   (lambda (x)
-                     (or (u:string-starts-with-p x "%ZED")
-                         (u:string-starts-with-p x "ZED")
-                         (u:string-starts-with-p x "CL-OPENGL")
-                         (string= x "SHADOW")
-                         (string= x "SDL2")))
-                   (mapcar #'package-name (list-all-packages)))))
-    `(if ,enable-p
-         (unwind-protect
-              (progn
-                (sb-profile:unprofile)
-                (sb-profile:profile ,@packages)
-                ,@body)
-           (sb-profile:report)
-           (sb-profile:unprofile)
-           (sb-profile:reset))
-         (progn ,@body))))
+  (if (member :sbcl *features*)
+      (let ((packages (remove-if-not
+                       (lambda (x)
+                         (or (u:string-starts-with-p x "%ZED")
+                             (u:string-starts-with-p x "ZED")
+                             (u:string-starts-with-p x "CL-OPENGL")
+                             (string= x "SHADOW")
+                             (string= x "SDL2")))
+                       (mapcar #'package-name (list-all-packages)))))
+        `(if ,enable-p
+             (unwind-protect
+                  (progn
+                    (sb-profile:unprofile)
+                    (sb-profile:profile ,@packages)
+                    ,@body)
+               (sb-profile:report)
+               (sb-profile:unprofile)
+               (sb-profile:reset))
+             (progn ,@body)))
+      `(progn
+         (warn "Profiling is only supported on SBCL.")
+         ,@body)))
