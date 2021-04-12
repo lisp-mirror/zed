bfe1f2d5da46846ac54a8eba9889de938c93c5d0
Michael Fiano <mail@mfiano.net>
2021-04-12 00:53:33 -0400

    Properly deregister contacting collider volumes

--- src/collision-system.lisp
+++ src/collision-system.lisp
@@ -5,7 +5,8 @@
             (:predicate nil)
             (:copier nil))
   (layers (u:dict #'eq) :type hash-table)
-  (grid nil :type hash-grid))
+  (grid nil :type hash-grid)
+  (contact-table (u:dict #'eq) :type hash-table))
 
 (u:define-printer (collision-system stream :type nil)
   (format stream "COLLISION-SYSTEM"))
@@ -84,32 +85,46 @@
         (layer2 (collision-volume-layer volume2))
         (source1 (collision-volume-source volume1))
         (source2 (collision-volume-source volume2)))
+    (remhash volume1 (collision-volume-contacts volume2))
+    (remhash volume2 (collision-volume-contacts volume1))
     (on-collision-exit layer1 source1 layer2 source2)
     (on-collision-exit layer2 source2 layer1 source1))
   nil)
 
-(u:fn-> compute-volume-contact (collision-volume collision-volume) null)
-(defun compute-volume-contact (volume1 volume2)
+(u:fn-> compute-volume-contact (collision-system collision-volume collision-volume) null)
+(defun compute-volume-contact (system volume1 volume2)
   (declare (optimize speed))
-  (let* ((collide-p (collide-p volume1 volume2))
-         (contacts1 (collision-volume-contacts volume1))
-         (contact-p (u:href contacts1 volume2)))
-    (cond
-      ((and collide-p contact-p)
-       (%on-collision-continue volume1 volume2))
-      ((and collide-p (not contact-p))
-       (setf (u:href contacts1 volume2) t
-             (u:href (collision-volume-contacts volume2) volume1) t)
-       (%on-collision-enter volume1 volume2))
-      ((and (not collide-p) contact-p)
-       (remhash volume2 contacts1)
-       (remhash volume1 (collision-volume-contacts volume2))
-       (%on-collision-exit volume1 volume2)))
+  (u:when-let ((collide-p (collide-p volume1 volume2)))
+    (let* ((contacts1 (collision-volume-contacts volume1))
+           (contact-p (u:href contacts1 volume2)))
+      (cond
+        ((and collide-p contact-p)
+         (%on-collision-continue volume1 volume2))
+        ((and collide-p (not contact-p))
+         (setf (u:href contacts1 volume2) t
+               (u:href (collision-volume-contacts volume2) volume1) t
+               (u:href (collision-system-contact-table system) volume1) volume2
+               (u:href (collision-system-contact-table system) volume2) volume1)
+         (%on-collision-enter volume1 volume2)))
+      nil)))
+
+(u:fn-> compute-volume-separations (collision-system) null)
+(defun compute-volume-separations (system)
+  (declare (optimize speed))
+  (let ((contacts (collision-system-contact-table system)))
+    (u:do-hash (volume1 volume2 contacts)
+      (unless (and
+               (collide-p volume1 volume2)
+               (trait-owner (collision-volume-collider volume1))
+               (trait-owner (collision-volume-collider volume2)))
+        (remhash volume1 contacts)
+        (remhash volume2 contacts)
+        (%on-collision-exit volume1 volume2)))
     nil))
 
-(u:fn-> %compute-collisions (hash-table vector) null)
+(u:fn-> %compute-collisions (collision-system hash-table vector) null)
 (declaim (inline %compute-collisions))
-(defun %compute-collisions (layers bucket)
+(defun %compute-collisions (system layers bucket)
   (declare (optimize speed))
   (when (>= (length bucket) 2)
     (u:map-combinations
@@ -118,7 +133,7 @@
              (volume2 (svref x 1)))
          (u:when-let ((layer (u:href layers (collision-volume-layer volume1))))
            (when (u:href layer (collision-volume-layer volume2))
-             (compute-volume-contact volume1 volume2)))))
+             (compute-volume-contact system volume1 volume2)))))
      bucket
      :length 2
      :copy nil)
@@ -131,6 +146,8 @@
          (layers (collision-system-layers system)))
     (map nil
          (lambda (x)
-           (%compute-collisions layers x)
+           (%compute-collisions system layers x)
            (setf (fill-pointer x) 0))
-         (hash-grid-buckets (collision-system-grid system)))))
+         (hash-grid-buckets (collision-system-grid system)))
+    (compute-volume-separations system)
+    nil))
