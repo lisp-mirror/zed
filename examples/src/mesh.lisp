c213b34b334b8e7d2cdd130811c0d3e93152fa48
Michael Fiano <mail@mfiano.net>
2021-04-21 16:48:41 -0400

    Revert "Test"
    This reverts commit 0351e870d281056ccc6da48e95af311c6618767b.

--- examples/src/mesh.lisp
+++ examples/src/mesh.lisp
@@ -26,16 +26,10 @@
               :use-ibl t)
    :features (:enable (:texture-cube-map-seamless))))
 
-(z:define-prefab mesh (:scale 10.0)
+(z:define-prefab mesh (:scale 32.0)
   (z.mesh:mesh :name "helmet"
                :asset '(:zed-examples "meshes/damaged-helmet.glb"))
-  (z.render:render :material 'mesh)
-  ((collider)
-   (z.collider:collider :layer 'layer
-                        :visible-p t)))
-
-(z:define-collision-plan mesh ()
-  (layer (layer)))
+  (z.render:render :material 'mesh))
 
 (z:define-prefab mesh-carousel (:template 'mesh
                                 :rotate-velocity (v3:velocity v3:+up+ const:+pi/6+)))
@@ -46,5 +40,4 @@
   (z:load-prefab core 'mesh-carousel))
 
 (z:define-context mesh ()
-  (:prelude #'mesh-prelude
-   :collision-plan 'mesh))
+  (:prelude #'mesh-prelude))
