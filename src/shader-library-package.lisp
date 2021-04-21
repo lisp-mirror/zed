c5c4118bd6d9a178861664604bc45f7cc6417bd3
Michael Fiano <mail@mfiano.net>
2021-04-20 21:32:17 -0400

    Add hashing functions to shader library

--- src/shader-library-package.lisp
+++ src/shader-library-package.lisp
@@ -116,6 +116,20 @@
    #:sine-in
    #:sine-in-out
    #:sine-out)
+  ;; Hashing functions
+  (:export
+   #:blum-blum-shub
+   #:blum-blum-shub/hq
+   #:fast32
+   #:fast32/2-per-corner
+   #:fast32/3-per-corner
+   #:fast32/4-per-corner
+   #:fast32/cell
+   #:fast32-2
+   #:fast32-2/4-per-corner
+   #:sgpp
+   #:sgpp/2-per-corner
+   #:sgpp/3-per-corner)
   ;; Programs
   (:export
    #:default
